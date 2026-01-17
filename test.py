5afeb53a65038a522f832cc80e1a396cfb2d03b4
Aadi2507 <Aadi2507>
2026-01-17 13:53:42 +0000

    Created class Car

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("Hello World")
-print("github")
+class Car:
+    
+    def __init__(self,engine,colour,model,brand):
+        self.engine = engine
+        self.colour = colour
+        self.model = model
+        self.brand = brand
+
+
+car = Car(3.0,"blue",2012,"BMW")
+print(car.model)
